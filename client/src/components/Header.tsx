6e9edce718181d06366fd2dd95c877f3a5b1892c
Wizzarddude305 <frankascencio27@gmail.com>
2026-03-24 17:47:56 -0400

    Formulating the Sign up Process for a new User along with quality of life changes

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,7 @@
+import SearchBar from "./SearchBar"
 
 function Header() {
-  return (
+  return (  
     <section className="header-section">
       <div className="header-content">
         <p className="eyebrow">University of Florida</p>
@@ -9,15 +10,7 @@ function Header() {
           CampusConnect helps students explore organizations, stay informed, and
           connect with campus opportunities.
         </p>
-
-        <div className="search-row">
-          <input
-            type="text"
-            placeholder="Search will be enabled with future event and organization data"
-            className="search-input"
-          />
-          <button className="search-button">Explore</button>
-        </div>
+          <SearchBar></SearchBar>
       </div>
     </section>
   )
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,29 @@
 import { useNavigate} from 'react-router-dom';
+import { useAuth } from '../pages/Login';
+import "../styles/navbar.css"
 
 function Navbar() {
   const navigate = useNavigate();
-
+  const {loggedIn, logout} = useAuth();
   return (
     <nav className="navbar">
       <div className="navbar-brand">CampusConnect</div>
-
       <div className="navbar-links">
         <a href="#">Home</a>
         <a href="#">Organizations</a>
         <a href="#">About</a>
       </div>
-
-      <button className="login-button" onClick={() => navigate('/login')}>
-        Student Login
-      </button>
+      {loggedIn ? (
+        <div>
+          <span>Welcome back!</span>
+          <button onClick={logout}>Log Out</button>
+        </div>
+      ) : (
+        <button className="login-button" onClick={() => navigate('/login')}>
+          Student Login
+        </button>
+      )}
+      
     </nav>
   )
 }
